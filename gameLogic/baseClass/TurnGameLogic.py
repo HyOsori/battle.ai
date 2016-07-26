f45f58fdefa2a4c7cc3e98dbc120d70e850ae4b7
Greedy <greedy0110@gmail.com>
2016-07-26 21:22:50 +0900

    add TurnGameLogic base class

---
+++ gameLogic/baseClass/TurnGameLogic.py
@@ -0,0 +1,45 @@
+import abc
+
+class TurnGameLogic:
+    __metaclass__ = abc.ABCMeta
+    def __init__(self, room, args):
+        self.room = room
+        self.phaseList = []
+        self.messageList = []
+        self.currentPhase = None
+
+    def onStart(self, args):
+        self.playerList = args
+        self.turnNum = -1
+        self.changeTurn()
+
+    def onAction(self,pid,args):
+        self.currentPhase.doAction(args)
+
+    @abc.abstractmethod
+    def onEnd(self):
+        pass
+
+    @abc.abstractmethod
+    def onError(self, pid):
+        pass
+
+    def changePhase(self, index):
+        if self.currentPhase != None:
+            self.currentPhase.onEnd()
+        self.currentPhase = self.phaseList[index]
+        self.currentPhase.onStart()
+        self.message = self.messageList[index]
+
+    def changeTurn(self):
+        self.turnNum = self.turnNum + 1
+
+    def changeTurn(self, index):
+        self.turnNum = index
+
+    def nowTurn(self):
+        length = len(self.playerList)
+        return self.playerList[self.turnNum%length]
+
+    def requset(self,pid,messageType,JSON):
+        self.room.requset(pid,messageType,JSON)
