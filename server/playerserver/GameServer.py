4155b36140226b7911962ed184ec1c7ac5353d53
eunjiChung <twih@naver.com>
2016-07-26 19:24:24 +0900

    Server is merged. Error is not corrected yet.

---
+++ server/playerserver/GameServer.py
@@ -0,0 +1,83 @@
+#-*- coding:utf-8 -*-
+
+import json
+import GameLogic
+
+from tornado import gen
+
+"""
+GAMESERVER
+
+1. WebServer에서 web_client_list, battle_ai_list, Room을 전달 받는다
+2. 생성자에서 게임 턴 순서 정하기 >> selectTurn() >> 순서 배열 리턴
+3. onStart(pid[]) >> 게임 시작
+4. request(pid, msgTyp, JSON게임판정보)으로 player와 logic에 전달
+   msgType에 따른 적합성 판단 -> 유효범위의 msgType인지
+5. onAction() 호출 >> 리턴값으로 유효성 전달받음
+6. onError(), onEnd() 호출
+7. 게임 진행에 따른 로그 저장 >> 일단 light하게 메모리에 저장
+
+"""
+
+class GameServer:
+    def __init__(self, room):
+        self.room = room
+        self.current_msgtype = -1
+
+
+    def selectTurn(self):
+        turn = []
+        self.perm(0)
+        return turn
+
+    def perm(self, num):
+        pass
+
+    @gen.coroutine
+    def __game_handler(self):
+        try:
+            turn = self.selectTurn(self.room.players)
+            GameLogic.onStart(turn)
+            for player in self.room.players:
+                self.__player_handler(player)
+        except:
+            GameLogic.onError()
+            print('[ERROR] GAME SET FAILED')
+        finally:
+            GameLogic.onEnd()
+
+
+    def request(self, player, msg, gameData):
+        self.current_msgtype = msg
+
+        ##send
+        data = { "msg_type" : msg , "game" : gameData}
+        json_data = json.dumps(data)
+        player.send(json_data)
+        for attendee in self.room.attendee_list:
+            attendee.send(json_data)
+
+
+    @gen.coroutine
+    def __player_handler(self, player):
+        while True:
+            message = yield player.read()
+            res = yield json.loads(message)
+            if res["msg_type"] == self.current_msgtype:
+                recv = yield GameLogic.onAction(player, message)
+                if not recv:
+                    raise Exception
+                else:
+                    for attendee in self.room.attendee_list:
+                        attendee.send(message)
+            elif res["msg_type"] == "end":  ## end는 종료 메세지 타입
+                break
+            else:
+                raise Exception
+
+
+    def save_game_data(self):
+        pass
+
+
+
---
+++ server/playerserver/PlayerServer.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+import tornado.tcpserver
+from server.User import User
+
+from tornado import gen
+
+
+class PlayerServer(tornado.tcpserver.TCPServer):
+    def __init__(self, web_client_list, battle_ai_list):
+        tornado.TCPServer.__init__(self)
+        self.battle_ai_list = battle_ai_list
+        self.web_client_list = web_client_list
+
+    def handle_stream(self, stream, address):
+        tornado.ioloop.IOLoop.current().spawn_callback(self.__accept_handler__, stream)
+
+    @gen.coroutine
+    def __accept_handler__(self, stream):
+        pid = stream.read_bytes(256, partial=True)
+        player = User.Player(pid, stream)
+
+        self.battle_ai_list.append(player)
+        for attendee in self.web_client_list:
+            attendee.notice_user_added(pid)
