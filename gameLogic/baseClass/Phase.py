4db09dad8517d30c8adfc9ce2353a04d5c3f5104
Hyeonseok Oh <hyeonso5@gmail.com>
2016-08-04 17:17:36 +0900

    Fixed TurnGameLogic little and add phase class

---
+++ gameLogic/baseClass/Phase.py
@@ -0,0 +1,38 @@
+class Phase(object):
+	def __init__(self, logicServer, messageType):
+		self._logicServer = logicServer
+		self.messageType = messageType
+		pass
+
+	def onStart(self):
+		pass
+
+	def doAction(self,pid, args):
+		pass
+
+	def onEnd(self):
+		pass
+
+	def changePhase(self, index):
+		self._logicServer.changePhase(index)
+
+	def changeTurn(self, index = None):
+		self._logicServer.changeTurn(index)
+
+	def nowTurn(self):
+		return self._logicServer.nowTurn()
+
+	def request(self, pid, JSON):
+		self._logicServer.request(pid, self.messageType, JSON)
+
+	def requestAll(self, JSON):
+		self._logicServer.requestAll(self.messageType, JSON)
+
+	def end(self, isValidEnd, resultList):
+		self._logicServer.end(isValidEnd, resultList)
+		
+	def getSharedDict(self):
+		return self._logicServer.getSharedDict()
+
+	def getPlayerList(self):
+		return self._logicServer.getPlayerList()
--- gameLogic/baseClass/TurnGameLogic.py
+++ gameLogic/baseClass/TurnGameLogic.py
@@ -1,45 +1,58 @@
 import abc
 
-class TurnGameLogic:
-    __metaclass__ = abc.ABCMeta
-    def __init__(self, room, args):
-        self.room = room
-        self.phaseList = []
-        self.messageList = []
-        self.currentPhase = None
+class TurnGameLogic(object):
+	__metaclass__ = abc.ABCMeta
+	def __init__(self, room):
+		self._room = room
+		self._phaseList = []
+		self._currentPhase = None
+		self._sharedDict = {}
 
-    def onStart(self, args):
-        self.playerList = args
-        self.turnNum = -1
-        self.changeTurn()
+	def onStart(self, playerList):
+		self._playerList = playerList
+		self._turnNum = -1
+		self.changeTurn()
 
-    def onAction(self,pid,args):
-        self.currentPhase.doAction(args)
+	def onAction(self,pid,JSON):
+		self._currentPhase.doAction(pid, JSON)
 
-    @abc.abstractmethod
-    def onEnd(self):
-        pass
+	@abc.abstractmethod
+	def onError(self, pid):
+		pass
 
-    @abc.abstractmethod
-    def onError(self, pid):
-        pass
+	def changePhase(self, index):
+		if self._currentPhase != None:
+			self._currentPhase.onEnd()
+		self._currentPhase = self._phaseList[index]
+		self._messageType = self._currentPhase.messageType
+		self._currentPhase.onStart()
 
-    def changePhase(self, index):
-        if self.currentPhase != None:
-            self.currentPhase.onEnd()
-        self.currentPhase = self.phaseList[index]
-        self.currentPhase.onStart()
-        self.message = self.messageList[index]
+	def changeTurn(self, index = None):
+		if index != None:
+			self._turnNum = index
+		else:
+			self._turnNum = self._turnNum + 1
 
-    def changeTurn(self):
-        self.turnNum = self.turnNum + 1
+	def nowTurn(self):
+		length = len(self._playerList)
+		return self._playerList[self._turnNum%length]
 
-    def changeTurn(self, index):
-        self.turnNum = index
+	def requestAll(self, messageType, JSON):
+		for pid in self._playerList:
+			self.request(pid, messageType, JSON)
 
-    def nowTurn(self):
-        length = len(self.playerList)
-        return self.playerList[self.turnNum%length]
+	def request(self, pid, messageType, JSON):
+		self._room.request(pid, messageType, JSON)
 
-    def requset(self,pid,messageType,JSON):
-        self.room.requset(pid,messageType,JSON)
+	def end(self, isValidEnd, resultList):
+		self._room.onEnd(isValidEnd, resultList)
+
+	def appendPhase(self, phase):
+		self._phaseList.append(phase)
+		return len(self._phaseList)-1
+		
+	def getSharedDict(self):
+		return self._sharedDict
+
+	def getPlayerList(self):
+		return self._playerList
