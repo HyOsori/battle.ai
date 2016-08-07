c01bca8fb67be965ceb2fe2dfc817e94692aec1a
wrlawodms <wrlawodms@gmail.com>
2016-08-07 21:22:45 +0900

    modify baskin client

--- static/ingame_baskin.js
+++ static/ingame_baskin.js
@@ -1,5 +1,9 @@
 var baskinMessgae = document.getElementById('id_baskin_message');
 var cnt = 0;
+var users;
+var users_color = ["Peru", "Pink", "Plum", "PowderBlue", "Purple", "Red", "RosyBrown",
+                    "ROyalBlue", "SaddleBrown","Salmon"];
+var users_win_count = []
 
 var gameMessage = "베스킨라빈스써리원귀엽고깜찍하게써리원" + "<br>";
 baskinMessgae.innerHTML = gameMessage;
@@ -7,6 +11,7 @@ baskinMessgae.innerHTML = gameMessage;
 
 var canvas = $("#id_board_canvas")[0];
 var ctx = canvas.getContext("2d");
+ctx.font = "10px Arial";
 
 function drawCircle(x,y,color){//draw stone
     ctx.beginPath();
@@ -17,43 +22,66 @@ function drawCircle(x,y,color){//draw stone
         ctx.fillStyle = "black"
         ctx.fill();
     }
-    else {
-        if (color == 2){
-            ctx.arc(35+(50*x),35+(50*y),20,0,endAngle);
-            ctx.fillStyle = "white"
-            ctx.fill();
-            ctx.strokeStyle = "black"
-            ctx.stroke();
-        }
-        else {//erase stone by drawing rect filled with background color
-            if (color == 0)
-                ctx.clearRect(11+(50*x),11+(50*y),48,48);
-        }
+    else if (color == 2){
+        ctx.arc(35+(50*x),35+(50*y),20,0,endAngle);
+        ctx.fillStyle = "white"
+        ctx.fill();
+        ctx.strokeStyle = "black"
+        ctx.stroke();
+    }else if(color == 0){//erase stone by drawing rect filled with background color
+        ctx.clearRect(11+(50*x),11+(50*y),48,48);
+    }else{
+        ctx.arc(35+(50*x),35+(50*y),20,0,endAngle);
+        ctx.fillStyle = color
+        ctx.fill();
     }
+
     ctx.closePath();
 }
 
-for(var i=0; i<5; i++) {
-    for(var j=0; j<6; j++) {
-        drawCircle(j, i, 2)
+for(var i=0; i<6; i++) {
+    for(var j=0; j<5; j++) {
+        drawCircle(j, i, 2);
     }
 }
 drawCircle(0, 5, 2)
 
+function gameStart(user_list) {
+    users = user_list;
+    for(var i=0; i< users.length(); i++){
+        drawCircle(8,i, users_color[i]);
+        ctx.fillText(users[i], 430, 50*i);
+    }
+    // for(var user in users){
+    //     drawCircle(8,0)
+    // }
+    //user 색 설정
+    //user 색 및 플레이어 이름 출력
+}
 
+function getIndexFromData(data, l) {
+    for(i=0; i<l.length(); i++) {
+        if (l[i] == data)
+            return i;
+    }
+}
 
 function recvGameMsg(data) {
     if (data.msg_type == "notify_gameloop") {
-        gameMessage += data.game_data.pid + " : ";
+        // gameMessage += data.game_data.pid + " : ";
+        user_color = users_color[getIndexFromData(data.game_data.pid, users)]
         for( var i = 0; i < data.game_data.num; i++ )
         {
             cnt++;
-            gameMessage += cnt + " ";
+            // gameMessage += cnt + " ";
+            drawCircle(cnt%5+1, cnt/5+1, user_color)
         }
-        gameMessage += "<br>";
-        baskinMessgae.innerHTML = gameMessage;
+        // gameMessage += "<br>";
+        // baskinMessgae.innerHTML = gameMessage;
     }
     else if (data.msg_type == "round_result") {
+
+
         gameMessage += "-------------------------" + "<br>";
         for (var key in data.game_data )
         {
