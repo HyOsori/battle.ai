97462f9e965289a15c47f685a58e1a3a3170af67
ZombieKim <mrbin2002@naver.com>
2016-08-07 20:48:24 +0900

    complete front of baskin

--- static/ingame_baskin.js
+++ static/ingame_baskin.js
@@ -28,10 +28,20 @@ function recvGameMsg(data) {
         }
         gameMessage += "-------------------------" + "<br>";
         baskinMessgae.innerHTML = gameMessage;
-        
+
         cnt = 0;
     }
 }
 function recvGameResult(data) {
-
+    gameMessage += "<br>";
+    for (var key in data.game_data )
+    {
+        gameMessage += key + " ";
+        if (data.game_data[key] == "win")
+            gameMessage += "최종 승리";
+        else
+            gameMessage += "최종 패배";
+        gameMessage += "<br>";
+    }
+    baskinMessgae.innerHTML = gameMessage;
 }
