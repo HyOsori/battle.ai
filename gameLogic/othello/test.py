235849f3495adf2f496db44f16b7e2f94dcd787c
Greedy <greedy0110@gmail.com>
2016-09-11 23:41:14 +0900

    test commit

--- gameLogic/othello/test.py
+++ gameLogic/othello/test.py
@@ -1,14 +1,16 @@
 #-*-coding:utf-8-*-
+import sys
 from gameLogic.baseClass.Client import Client
 from gameLogic.othello.myOthelloParser import MyOthelloParser
 
 HOST = '127.0.0.1'
 PORT = 9001
 
-client = Client(HOST,PORT)
+client = Client()
+if client.conntectServer(HOST,PORT) == False:
+    print '서버 연결오류'
+    sys.exit()
 test1 = MyOthelloParser()
 client.setParser(test1)
 
 client.clientRun()
-
-print "finish!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
