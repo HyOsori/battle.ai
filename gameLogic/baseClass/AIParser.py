6ef537698d6e2f0dd42ddeefa6a52338fbcfc7c9
Greedy <greedy0110@gmail.com>
2016-10-31 20:10:44 +0900

    temp

--- gameLogic/baseClass/AIParser.py
+++ gameLogic/baseClass/AIParser.py
@@ -27,7 +27,7 @@ class AIParser:
         pass
 
     def make_send_msg(self, msg_type, game_data):
-        send_msg = {"msg": "game_data"}
+        send_msg = {"msg": "data"}
         send_msg["msg_type"] = msg_type
         send_msg["game_data"] = game_data
         return send_msg
