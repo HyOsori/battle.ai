658ea9bb1b8854b8326ef9f40bb4413989bf79f8
Yeong-uk Ko <greenknite@naver.com>
2016-11-01 12:46:20 +0900

    MyPixelsParser2 완료. (랜덤보다 쪼금 쎈 애)

--- gameLogic/pixels/MyPixelsParser2.py
+++ gameLogic/pixels/MyPixelsParser2.py
@@ -24,35 +24,27 @@ class MyPixelsParser2(PixelsParser):
         elif ruler_self == 2:
             my_color = color_array[start_point_x[1]][start_point_y[1]]
 
-        '''
-        for y in range(HEIGHT):
-            for x in range(WIDTH):
-                if (RulerArray[y][x] == 0 and (
-                        # If the area isn't ruled
-                                    (x > 0 and RulerArray[y][x - 1] == ruler) or  # Check left side
-                                    (x < (WIDTH - 1) and RulerArray[y][x + 1] == ruler) or  # Check right side
-                                (y > 0 and RulerArray[y - 1][x] == ruler) or  # Check up side
-                            (y < (HEIGHT - 1) and RulerArray[y + 1][x] == ruler))):  # Check down side
-                    NumOfEachColor[colornumarray[y][x]] = NumOfEachColor[
-                                                              colornumarray[y][x]] + 1  # We can rule the area
+        for y in range(height):
+            for x in range(width):
+                if (ruler_array[y][x] == 0) and (
+                    (x > 0 and ruler_array[y][x - 1] == ruler_self) # Check left side
+                    or (x < (width - 1) and ruler_array[y][x + 1] == ruler_self) # Check right side
+                    or (y > 0 and ruler_array[y - 1][x] == ruler_self) # Check up side
+                    or (y < (height - 1) and ruler_array[y + 1][x] == ruler_self) # Check down side
+                ):  # We can rule the area. Divide according to colors and count the number.
+                    num_of_each_color[color_array[y][x]] += 1
 
         max = 0
-        returncolornum = random.randint(1, 6)
-        while (returncolornum == exclusionnum or returncolornum == mycolornum):
-            returncolornum = random.randint(1, 6)
-        NumOfEachColor[0] = -1
-
-        for i in range(NumOfColor + 1):
-            if (NumOfEachColor[i] > max and i != mycolornum and i != exclusionnum):
-                max = NumOfEachColor[i]
-                returncolornum = i
-
-        return returncolornum
-        '''
 
         return_color = random.randint(1, 6)
         while return_color == enemy_chosen_color or return_color == my_color:
             return_color = random.randint(1, 6)
+
+        num_of_each_color[0] = -1
+        for i in range(num_of_color + 1):
+            if num_of_each_color[i] > max and i != my_color and i != enemy_chosen_color:
+                max = num_of_each_color[i]
+                return_color = i
 
         parsing_data = {'chosen_color': return_color}
 
