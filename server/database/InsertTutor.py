c47d53a1af0ad8d033018e81e84b169ecf231817
Dujin Jeong <wjdenwls123@gmail.com>
2016-11-03 00:31:17 +0900

    database tuple to array, protocol

--- server/database/InsertTutor.py
+++ server/database/InsertTutor.py
@@ -7,3 +7,6 @@ db_base = LogDB()
 # default cnt = 1, name = 'none', win_lose = 'all'
 # print LogDB.search_game_log(db_base,cnt=3,name='hi',win_lose='win');
 # print LogDB.search_game_log(db_base)
+
+# message = {MSG: GAME_LOG, MSG_TYPE: REQUEST_LOG_DATA, DATA: [cnt, name, win_lose]}
+# message = {MSG: GAME_LOG, MSG_TYPE: RESPONSE_LOG_DATA, DATA: [[winner, win_score, loser, lose_score]]}
