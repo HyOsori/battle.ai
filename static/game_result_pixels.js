43d81b7d5a507fd20979974f78942bfc25769d49
kimjieun02 <laurelis37@gmail.com>
2016-11-05 20:51:24 +0900

    spread gameresult li

--- static/game_result_pixels.js
+++ static/game_result_pixels.js
@@ -1,26 +1,37 @@
 var gameResults = [];
 
-function DrawResultBoard() {
+function DrawResultBoard(index) {
     var nav = document.getElementById('id_gameResults_ul');
     var index = 0;
-    for(index; index<nav.childNodes.length; index++){
-        var child = nav.childNodes[index];
-        if (child.className == 'class_selected') {
-            break;
-        }
-    }
+
     DrawBoard(gameResults[index]["round1"]["board"]);
 }
 
 function SaveRoundResult(winner){
     $('<li />').bind('click', function(event) {
         var nav = document.getElementById('id_gameResults_ul');
+        var index = 0;
         for (var i=0; i<nav.childNodes.length; i++){
             var child = nav.childNodes[i];
+            if (child.className == 'class_selected') {
+                child.innerHTML = 'Round ' + round + ' ' + gameResults[i]["winner"];
+            }
             child.className = '';
         }
+        for(index; index<nav.childNodes.length; index++){
+            var child = nav.childNodes[index];
+            if (child.className == 'class_selected') {
+                break;
+            }
+        }
         event.target.className = 'class_selected';
-        DrawResultBoard();
+        var player1 = gameResults[index]["round1"]["player"]["player1"];
+        var player2 = gameResults[index]["round1"]["player"]["player2"];
+        var player1_2r = gameResults[index]["round2"]["player"]["player1"];
+        var player2_2r = gameResults[index]["round2"]["player"]["player2"];
+        $(this).append('<br>', player1[0], " ", player1[1], " : ", player2[1], " ", player2[0],
+                        '<br>', player1[0], " ", player1[1], " : ", player2[1], " ", player2[0]);
+        DrawResultBoard(index);
     }).append('Round ', round, ' ', winner).appendTo('#id_gameResults_ul')
 }
 
