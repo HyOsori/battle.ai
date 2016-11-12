8d7823083e0285c1e005322baadbdb6c33c9c0b4
bigs <wrlawodms@gmail.com>
2016-11-12 03:34:39 +0000

    dummy setting

--- server/dummy_client/attach_dummy.py
+++ server/dummy_client/attach_dummy.py
@@ -1,8 +1,12 @@
+import sys
+sys.path.insert(0, "/home/bigs/battle.ai/")
+
+print sys.path
+
+
+
 from multi_dummy import DummyManager
 from gameLogic.pixels.MyPixelsParser2 import MyPixelsParser2
-
-import sys
-sys.path.insert(0, "/home/bigs/battle.ai")
 
 dm = DummyManager(MyPixelsParser2(), 'Dummy3')
 dm.attach('127.0.0.1', 9001)
