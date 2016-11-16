c408c6d375ee2015cd16dff4c2e19f9c7ffbcca0
junsuLime <vfv0121@gmail.com>
2016-11-16 23:21:05 +0900

    dummy error modify

--- server/dummy_client/attach_dummy.py
+++ server/dummy_client/attach_dummy.py
@@ -6,7 +6,7 @@ print sys.path
 from multi_dummy import DummyManager
 from gameLogic.pixels.MyPixelsParser2 import MyPixelsParser2
 
-dm = DummyManager(MyPixelsParser2(), 'Dummy3')
+dm = DummyManager(MyPixelsParser2, 'Dummy3')
 dm.attach('104.199.218.103', 9001)
 
 
