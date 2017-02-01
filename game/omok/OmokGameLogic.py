e73b8080ffcdc32d0bce92898f32b3e24307d0cb
Dujin Jeong <wjdenwls123@gmail.com>
2017-02-01 22:06:38 +0900

    feat: update omok
    until init

--- game/omok/OmokGameLogic.py
+++ game/omok/OmokGameLogic.py
@@ -1,6 +1,5 @@
 import base64
 import json
-import logging
 import random
 import sys
 import zlib
@@ -9,13 +8,17 @@ from gamebase.game.Phase import Phase
 
 from gamebase.game.TurnGameLogic import TurnGameLogic
 
+import game.debugger as logging
+
 sys.path.insert(0, '../')
-logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
 
 class OmokGameLogic(TurnGameLogic):
     def __init__(self, game_server):
         super(OmokGameLogic, self).__init__(game_server)
         logging.debug('GameLogic : INIT')
+        self.width = 20
+        self.height = 20
 
         # Declare color_array_init.
         self.board = [[0 for x in range(self.width)] for y in range(self.height)]
@@ -43,118 +46,122 @@ class OmokGameLogic(TurnGameLogic):
         logging.debug('OmokGameLogic -> LoopPhase')
         self.change_phase(0)
 
+
 class OmokLoopPhase(Phase):
     def __init__(self, logic_server, message_type):
         super(OmokLoopPhase, self).__init__(logic_server, message_type)
         logging.debug('PHASE_LOOP : INIT')
 
-        def on_start(self):
-            super(OmokLoopPhase, self).on_start()
-            logging.debug('PHASE_LOOP : START')
+    def on_start(self):
+        super(OmokLoopPhase, self).on_start()
+        logging.debug('PHASE_LOOP : START')
 
-            # Initialize variables from outside.
-            self.player_list = self.get_player_list()
-            self.shared_dict = self.get_shared_dict()
-            self.next_phase = self.shared_dict['PHASE_FINISH']
-            self.width = self.shared_dict['width']
-            self.height = self.shared_dict['height']
-            self.board = self.shared_dict['board']
+        # Initialize variables from outside.
+        self.player_list = self.get_player_list()
+        self.shared_dict = self.get_shared_dict()
+        self.next_phase = self.shared_dict['PHASE_FINISH']
+        self.width = self.shared_dict['width']
+        self.height = self.shared_dict['height']
+        self.board = self.shared_dict['board']
 
-            # Initialize variables.
-            self.round = 0  # Check Rounds.
-            self.initialize = False  # Initialize arrays if new round starts.
+        # Initialize variables.
+        self.round = 0  # Check Rounds.
+        self.initialize = False  # Initialize arrays if new round starts.
 
-            self.notify_to_front_init()
+        self.notify_to_front_init()
 
-            self.change_turn(0)
-            self.request_to_client(1, 2)
+        self.change_turn(0)
+        self.request_to_client(1, 2)
 
-        def do_action(self, pid, dict_data):
-            super(OmokLoopPhase, self).do_action(pid, dict_data)
-            # logging.debug('PHASE_LOOP : DO_ACTION / pid : ' + pid)
+    def do_action(self, pid, dict_data):
+        super(OmokLoopPhase, self).do_action(pid, dict_data)
+        # logging.debug('PHASE_LOOP : DO_ACTION / pid : ' + pid)
 
-            ruler = 0
-            if pid == self.player_list[0]:
-                ruler = 1
-            if pid == self.player_list[1]:
-                ruler = 2
+        ruler = 0
+        if pid == self.player_list[0]:
+            ruler = 1
+        if pid == self.player_list[1]:
+            ruler = 2
 
-            if self.chosen_location == dict_data['chosen_location']:
-                result = dict(zip(self.player_list, ['win'] * len(self.player_list)))
-                result[pid] = 'lose'
-                logging.error(pid + ' invalid Chosen')
-                self.end(False, result)
-                return
-
-            self.absorb(ruler, self.chosen_color)
-            self.notify_to_front(ruler)
-
-            if self.check_status():  # If check_status returns True, the round is finished.
-                self.notify_to_front_change_round()
-                self.round += 1
-
-                self.initialize = True
-
-            if self.initialize:  # Initialize the arrays if new(2nd) round starts.
-                self.board = [[0 for x in range(self.width)] for y in range(self.height)]
-                self.initialize = False
-
-                if pid == self.player_list[1]:
-                    self.change_turn()
-
-            if self.round == 2:  # Change phase if two rounds are finished.
-                self.shared_dict['score'] = self.score
-                # logging.debug('LoopPhase -> FinishPhase')
-                self.change_phase(self.next_phase)
-                return
-
-            ruler_enemy = ruler  # Ruler who finished absorbing
-            ruler_self = ruler % 2 + 1  # Ruler who will take the request
-
-            self.change_turn()
-            self.request_to_client(ruler_self, ruler_enemy)
-
-        def on_end(self):
-            super(OmokLoopPhase, self).on_end()
-            # logging.debug('PHASE LOOP : ON_END')
-
-        def notify_to_front_init(self):
-            notify_dict = {
-                'width': self.width,
-                'height': self.height,
-                'board': json.dumps(self.board)
-            }
-            self.notify_init(notify_dict)
-
-        def notify_to_front(self, ruler):
-            notify_dict = {
-                'ruler_who': ruler,  # Who just finished absorbing
-                'chosen_location': self.chosen_location,
-                'score': self.score
-            }
-            self.notify(notify_dict)
-
-        def notify_to_front_change_round(self):
-            print('notify to front end round')
-            notify_dict = {
-                'round': self.round,
-                'first': self.player_list[self.round],
-                'second': self.player_list[1 - self.round]
-            }
-            self.notify_free('notify_change_round', notify_dict)
-
-        def request_to_client(self, ruler_self, ruler_enemy):
-            # logging.debug('Request ' + self.now_turn() + '\'s decision')
-            info_dict = {
-                'ruler_self': ruler_self,
-                'ruler_enemy': ruler_enemy,
-                'enemy_chosen_location': self.chosen_location
-            }
-            self.request(self.now_turn(), info_dict)
-
-        def absorb(self, ruler, location):
+        if self.chosen_location == dict_data['chosen_location']:
+            result = dict(zip(self.player_list, ['win'] * len(self.player_list)))
+            result[pid] = 'lose'
+            logging.error(pid + ' invalid Chosen')
+            self.end(False, result)
             return
 
+        self.absorb(ruler, self.chosen_color)
+        self.notify_to_front(ruler)
+
+        if self.check_status():  # If check_status returns True, the round is finished.
+            self.notify_to_front_change_round()
+            self.round += 1
+
+            self.initialize = True
+
+        if self.initialize:  # Initialize the arrays if new(2nd) round starts.
+            self.board = [[0 for x in range(self.width)] for y in range(self.height)]
+            self.initialize = False
+
+            if pid == self.player_list[1]:
+                self.change_turn()
+
+        if self.round == 2:  # Change phase if two rounds are finished.
+            self.shared_dict['score'] = self.score
+            # logging.debug('LoopPhase -> FinishPhase')
+            self.change_phase(self.next_phase)
+            return
+
+        ruler_enemy = ruler  # Ruler who finished absorbing
+        ruler_self = ruler % 2 + 1  # Ruler who will take the request
+
+        self.change_turn()
+        self.request_to_client(ruler_self, ruler_enemy)
+
+    def on_end(self):
+        super(OmokLoopPhase, self).on_end()
+        # logging.debug('PHASE LOOP : ON_END')
+
+    def notify_to_front_init(self):
+        notify_dict = {
+            'width': self.width,
+            'height': self.height,
+            'board': json.dumps(self.board)
+        }
+        self.notify_init(notify_dict)
+
+    def notify_to_front(self, ruler):
+        notify_dict = {
+            'ruler_who': ruler,  # Who just finished absorbing
+            'chosen_location': self.chosen_location,
+            'score': self.score
+        }
+        self.notify(notify_dict)
+
+    def notify_to_front_change_round(self):
+        print('notify to front end round')
+        notify_dict = {
+            'round': self.round,
+            'first': self.player_list[self.round],
+            'second': self.player_list[1 - self.round]
+        }
+        self.notify_free('notify_change_round', notify_dict)
+
+    def request_to_client(self, ruler_self, ruler_enemy):
+        # logging.debug('Request ' + self.now_turn() + '\'s decision')
+        info_dict = {
+            'ruler_self': ruler_self,
+            'ruler_enemy': ruler_enemy,
+            'enemy_chosen_location': self.chosen_location
+        }
+        self.request(self.now_turn(), info_dict)
+
+    def request_to_client(self, ruler_self, ruler_enemy):
+        # logging.debug('Request ' + self.now_turn() + '\'s decision')
+        info_dict = {
+            'board': json.dumps(self.board)
+        }
+        self.request(self.now_turn(), info_dict)
 
 
 class OmokFinishPhase(Phase):
--- game/omok/OmokParser.py
+++ game/omok/OmokParser.py
@@ -3,17 +3,103 @@ import base64
 import json
 import sys
 import zlib
+from gamebase.client.AIParser import AIParser
 
 sys.path.insert(0,'../')
-from gamebase.client.AIParser import AIParser
+
 
 class OmokParser(AIParser):
     def __init__(self):
-        pass
+        """
+        :return: None
+        """
+        self._client = None
+        self.pid = None
+        self.msg = None
+        self.msg_type = None
+        self.game_data = None
 
-    def loop_phase(self):
+    def init_parser(self, client, username):
         """
-        must override
-        :return:
+        :param client: Client class, client calls this function to initialize parser
+        :param username: client's username
+        :return: None
         """
-        pass
+        self._client = client
+        self.pid = username
+
+    # game_data 로는 room에서 보낸 json이 loads된 상태의 dict 자료형이 반환된다.
+    # dict자료형에 대한 정보는 문서를 참조하며, 그 dict의 정보를 활용하여 logic을 실행 시킨다.
+    # 그리고 반환형에 맞춰서 정보를 조작한후 리턴한다.
+
+    # 고로 이 부분이 사용자가 코딩할 부분!
+
+    def decoding(self, decoding_data):
+        """
+        :param decoding_data: received data from server (dict)
+        :return: None
+        """
+        print(decoding_data)
+        self.msg = decoding_data['msg']
+        self.msg_type = decoding_data['msg_type']
+        self.game_data = decoding_data['data']
+
+
+    def parsing_data(self, decoding_data):
+        print("parsing_data is called")
+        base = super(OmokParser, self).parsing_data(decoding_data)
+        print("super.parsing_data is called")
+        print("self.msg_type: " + self.msg_type)
+        ret = None
+        if self.msg_type == 'loop':
+            ret = self.loop_phase()
+        if self.msg_type == 'notify_init_loop':
+            ret = self.notify_loop_init()
+        if self.msg_type == 'notify_change_round':
+            ret = self.notify_change_round()
+        if ret == None:
+            return base
+        else:
+            return self.make_send_msg(self.msg_type, ret)
+
+    @staticmethod
+    def make_send_msg(msg_type, game_data):
+        """
+        :param msg_type: Type of message to send (string)
+        :param game_data: Game data to send(dict)
+        :return: response of AI client (dict)
+        """
+        # 서버와 통신이 가능한 프로토콜로 포장하는 역할을 하는 메소드
+        send_msg = {"msg": "game_data"}
+        if msg_type == 'ready':
+            send_msg["msg"] = "game_handler"
+        send_msg["msg_type"] = msg_type
+        send_msg["data"] = game_data
+        print(send_msg)
+        return send_msg
+
+    def notify_loop_init(self):
+        print('notify_loop_init get!')
+        self.width = self.game_data['width']
+        self.height = self.game_data['height']
+        self.board = [[0 for x in range(self.width)] for y in range(self.height)]
+        return None
+
+
+    def init_phase(self):
+        """
+        :return: response of AI client (dict)
+        """
+        parsing_data = dict()
+        parsing_data['response'] = 'OK'
+        send_msg = self.make_send_msg(self.msg_type, parsing_data)
+        return send_msg
+
+    def finish_phase(self):
+        """
+        :return: response of AI client (dict)
+        """
+        parsing_data = dict()
+        parsing_data['response'] = 'OK'
+        send_msg = self.make_send_msg(self.msg_type, parsing_data)
+        return send_msg
