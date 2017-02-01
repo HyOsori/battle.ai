e616a5393a9463766ffa92a06237416f7fbf6081
junsuLime <vfv0121@gmail.com>
2017-02-01 21:50:15 +0900

    trash ai for omok
    To test omok game code

--- game/omok/MyOmokParser.py
+++ game/omok/MyOmokParser.py
@@ -2,9 +2,30 @@ from game.omok.OmokParser import OmokParser
 
 
 class MyOmokParser(OmokParser):
-    def loop_phase(self):
-        data = "result"
+    def __init__(self):
+        '''
+        Develop own your omok AI
+        width and height are board's width and height
+        empty place is represented integer 0
+        and your color is represented by number 0 1 2 ...
 
+        your color is set automatically.
+        '''
+        self.width = 0
+        self.height = 0
+        self.my_color = 0
+        pass
 
-        return data
+    def loop_phase(self, board):
+        '''
+        :param board: 2 dimension array that have omok board information
+        :return: return value is output sending data.
+                    in this game,
+                    if you want to put omok stone at board[x][y],
+                    output is ...    dict -  { x: n, y: m }
+        '''
+        for x in range(self.width):
+            for y in range(self.height):
+                if board[x][y] == 0:
+                    return {"x": x, "y": y}
 
