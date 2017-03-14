2b23706e20f5b54c938ddf63cfdb67c912f22993
2014005123 <jdj1211@hanyang.ac.kr>
2017-03-14 21:05:46 +0900

    feat: Base Parser
    define class

---
+++ game/alkaki/AlkakiParser.py
@@ -0,0 +1,14 @@
+# -*-coding:utf-8-*-
+import sys
+from gamebase.client.AIParser import AIParser
+import game.debugger as logging
+
+sys.path.insert(0, '../')
+
+
+class ALKAKIParser(AIParser):
+    def __init__(self):
+        pass
+
+    def init_phase(self, init_data):
+        pass
