6c00771312d59cd0fcd174aa30dc09575ebcb34f
2014005123 <jdj1211@hanyang.ac.kr>
2017-03-14 21:15:51 +0900

    feat: Base GameLogic
    define class

---
+++ game/alkaki/AlkakiGameLogic.py
@@ -0,0 +1,18 @@
+import sys
+
+from gamebase.game.Phase import Phase
+from gamebase.game.TurnGameLogic import TurnGameLogic
+import game.debugger as logging
+
+sys.path.insert(0, '../')
+
+
+class ALKAKIGameLogic(TurnGameLogic):
+    def __init__(self):
+        pass
+
+    def on_ready(self, player_list):
+        pass
+
+    def on_start(self):
+        pass
