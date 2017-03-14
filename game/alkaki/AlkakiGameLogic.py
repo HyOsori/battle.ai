61ccbeb46308b3c352a22724b1ce337e42e446c6
2014005123 <jdj1211@hanyang.ac.kr>
2017-03-14 21:42:10 +0900

    feat: GameLogic init, on_ready
    init- initialize game dependent variable
    
    on_ready- send server dict that made with init variable for multi player

--- game/alkaki/AlkakiGameLogic.py
+++ game/alkaki/AlkakiGameLogic.py
@@ -8,11 +8,20 @@ sys.path.insert(0, '../')
 
 
 class ALKAKIGameLogic(TurnGameLogic):
-    def __init__(self):
-        pass
+    def __init__(self, game_server):
+        super(ALKAKIGameLogic, self).__init__(game_server)
+        # Here init game dependent variable
 
     def on_ready(self, player_list):
-        pass
+        self._player_list = player_list
+
+        # Here makes dict for multi player init variable
+        init_dict = {}
+        for i in player_list:
+            init_dict[i] = {}
+
+        # Send Server
+        self._game_server.on_init_game(init_dict)
 
     def on_start(self):
         pass
