e773ed5ecf8913451365a0bba4ea42b7d690f93a
2014005123 <jdj1211@hanyang.ac.kr>
2017-03-23 00:32:30 +0900

    feat: on_start, do_start declare
    Server on_start, Phase do_start
    
    흐름 수정, turn 순서대로 전송 가능

--- game/alkaki/AlkakiGameLogic.py
+++ game/alkaki/AlkakiGameLogic.py
@@ -19,18 +19,15 @@ class ALKAKIGameLogic(TurnGameLogic):
         init_dict = {}
         for i in player_list:
             init_dict[i] = {}
-
         # Send Server
         self._game_server.on_init_game(init_dict)
 
     def on_start(self):
         # shared_dict-init out of this class(Phase)
         shared_dict = self.get_shared_dict()
-
         # Register Phase (phase name free)
         game_phase = ALKAKIGamePhase(self, 'game')
         shared_dict['PHASE_GAME'] = self.append_phase(game_phase)
-
         # Transfer Phase GameLogic -> GamePhase
         self.change_phase(0)
 
@@ -43,23 +40,19 @@ class ALKAKIGamePhase(Phase):
         self.player_list = None
         self.shared_dict = None
 
-    def on_start(self):
-        super(ALKAKIGamePhase, self).on_start()
-
         # Init data
         self.player_list = self.get_player_list()
         self.shared_dict = self.get_shared_dict()
 
         # Init game independent data
 
+    def do_start(self):
         # Send server msg
         self.change_turn(0)
         self.request_to_server()
 
     def do_action(self, pid, dict_data):
-        # check user decision
-        super(ALKAKIGamePhase, self).do_action(pid, dict_data)
-
+        logging.info("do_action called")
         # validate_user
         validate_user = 0
         if pid == self.player_list[0]:
@@ -67,6 +60,7 @@ class ALKAKIGamePhase(Phase):
         elif pid == self.player_list[1]:
             validate_user = 2
 
+        self.change_turn()
         # Notify to Observer(Web) game data
         self.notify_to_observer()
         # Requests to Server(Handler) game data
@@ -79,6 +73,7 @@ class ALKAKIGamePhase(Phase):
         self.notify("game", notify_dict)
 
     def request_to_server(self):
+        logging.info('Request ' + self.now_turn() + '\'s decision')
         request_dict = {
             'data': 'test'
         }
