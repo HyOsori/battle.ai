14aa878b67e8b8418abe9aa815f8668260f47e2f
2014005123 <jdj1211@hanyang.ac.kr>
2017-03-23 00:34:43 +0900

    feat: GameParser parsing data
    Msg_type을 Phase 설정시 type과 같게 해야 통신이 가능

--- game/alkaki/AlkakiParser.py
+++ game/alkaki/AlkakiParser.py
@@ -13,5 +13,13 @@ class ALKAKIParser(AIParser):
     def init_phase(self, init_data):
         pass
 
+    def parsing_data(self, decoding_data):
+        base = super(ALKAKIParser, self).parsing_data(decoding_data)
+        logging.info("PARSING DATA")
+
+        ret = None
+        ret = self.game_phase()
+        return self.make_send_msg('game', ret)
+
     def game_phase(self):
         raise NotImplementedError
