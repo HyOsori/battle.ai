54cdebac9b7cf8e55d0d6ff6aa64ad5137067750
clucle <wjdenwls123@gmail.com>
2017-05-16 22:42:03 +0900

    docs: update API define
    RESTFUL API, WEB SOCKET API define basic

---
+++ docs/Game.java
@@ -0,0 +1,5 @@
+public class Game {
+	private String game_id;
+	private GameLog[] log_list;
+	private AIFile[] ai_list;
+}
---
+++ docs/player.java
@@ -0,0 +1,8 @@
+private class Player {
+	/* 로그인 후 사용자가 가지는 정보 */
+
+	private String id;
+	private String password;
+	private DateTime created_at;
+	private GameInfo[] game_info_list;
+}
