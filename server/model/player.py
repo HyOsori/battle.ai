5d18732c62d4e3b81262e77e8c81fd430b875336
dms613412 <dms613412@naver.com>
2017-07-06 21:31:55 +0900

    2

--- server/model/player.py
+++ server/model/player.py
@@ -2,5 +2,5 @@ class Player(object):
     def __init__(self, name, encrypted_password):
         self.name = name
         self.password = encrypted_password
-        self.created_at
+        self.created_at =""
         self.game_info_list = []
--- static/login.js
+++ static/login.js
@@ -1,34 +1,36 @@
-
 /*
-class Login extends React.Component{
-    render(){
+ class Login extends React.Component{
+ render(){
 
-        let style={
-            textAlign: 'center',
-            float:'right',
-            margin:'20px',
-            width:'30vw'
-        }
-        return(
-           // <div style="{style}"></div>
-            <h1>codelab</h1>
-        );
-    }
-}
-class App extends React.Component{
-  render(){
-    return(
-    <Login/>
-    );
-  }
-}*/
+ let style={
+ textAlign: 'center',
+ float:'right',
+ margin:'20px',
+ width:'30vw'
+ }
+ return(
+ // <div style="{style}"></div>
+ <h1>codelab</h1>
+ );
+ }
+ }
+ class App extends React.Component{
+ render(){
+ return(
+ <Login/>
+ );
+ }
+ }*/
 //
 var Sign_in = React.createClass({
-    render:function () {
-        return(
+    render: function () {
+        let style = {
+            float: 'right',
+            margin: '20px'
+        }
+        return (
             <div>
-                <h1>ㅗㅑ</h1>>
-                <button id="sign_in"style="float:right;margin:20px"type="button" class="btn btn-default navbar-btn">Sign in</button>
+                <button id="sign_in" type="button" class="btn btn-default navbar-btn">Sign in</button>
             </div>
         );
 
@@ -36,15 +38,38 @@ var Sign_in = React.createClass({
 
 });
 var Sign_up = React.createClass({
-    render:function () {
-        return(
+    render: function () {
 
-            <h1>안녕녕</h1>
-       );
+        return (
+            <div>
+                <h1>Sign Up</h1>
+                <form action="/auth/signup" method="POST">
+                    <div class="form-group">
+                        <label for="exampleInputName">Name</label>
+                        <input type="text" name="name" class="form-control" id="exampleInputName" placeholder="name"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" name="password" class="form-control" id="exampleInputPassword1"
+                               placeholder="password"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword2">Password confirm</label>
+                        <input type="password" name="password_confirm" class="form-control" id="exampleInputPassword2"
+                               placeholder="password confirm"/>
+                    </div>
+                     {document.getElementById('exampleInputPassword1').value==document.getElementById('exampleInputPassword1').value?'TRUE':'FALSE'}
+                    <button type="submit" class="btn btn-default navbar-btn">Sign up</button>
+                </form>
 
+            </div>
+        );
     }
-
 });
-React.render(<Sign_in/>,document.getElementById('sign_in'));
 
-React.render(<Sign_up/>,document.getElementById('sign_up'));
+        React.render(
+        <Sign_in/>
+        ,document.getElementById('sign_in'));
+        React.render(
+        <Sign_up/>
+        ,document.getElementById('sign_up'));
