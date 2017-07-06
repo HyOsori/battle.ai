dd17af6278916ec6bbcb09739ef634b5282be1e9
dms613412 <dms613412@naver.com>
2017-07-06 20:10:21 +0900

    1

--- static/login.js
+++ static/login.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
 
+/*
 class Login extends React.Component{
     render(){
-/*
+
         let style={
             textAlign: 'center',
             float:'right',
             margin:'20px',
             width:'30vw'
-        }*/
+        }
         return(
            // <div style="{style}"></div>
             <h1>codelab</h1>
@@ -23,6 +21,30 @@ class App extends React.Component{
     <Login/>
     );
   }
-}
+}*/
+//
+var Sign_in = React.createClass({
+    render:function () {
+        return(
+            <div>
+                <h1>ㅗㅑ</h1>>
+                <button id="sign_in"style="float:right;margin:20px"type="button" class="btn btn-default navbar-btn">Sign in</button>
+            </div>
+        );
 
-ReactDOM.render(<App/>,document.getElementById('sign'));
+    }
+
+});
+var Sign_up = React.createClass({
+    render:function () {
+        return(
+
+            <h1>안녕녕</h1>
+       );
+
+    }
+
+});
+React.render(<Sign_in/>,document.getElementById('sign_in'));
+
+React.render(<Sign_up/>,document.getElementById('sign_up'));
