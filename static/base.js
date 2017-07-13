4b49385819b5ce48c827f633e230d745501da733
dms613412 <dms613412@naver.com>
2017-07-13 22:25:12 +0900

    갓두

--- static/base.js
+++ static/base.js
@@ -2,29 +2,82 @@
  * Created by mypc on 2017-07-06.
  */
 
-
+var Body = React.createClass({
+    render: function(){
+        let style={
+            paddingTop:'23px'
+        }
+        return(
+            <div style={style}>
+            </div>
+        );
+    }
+})
 var Sign_in = React.createClass({
     render: function () {
+           let style={
+            position:'fixed',
+            width:'100%',
+            height:'70px',
+            backgroundColor:'#009688',
+            boxShadow:'0 1px 0 rgba(0,0,0,.10196078)',
+               padding:'10px',
+               zIndex:'999'
+
+        }
+        let style1={
+           float:'right'
+        }
 
         return (
-            <div>
+            <div style={style} >
+                <form className="form-inline" action="/auth/signin" method="POST" style={style1}>
+                  <div className="form-group">
 
-                <form action="/auth/signin" method="POST">
-  <div class="form-group">
-
-    <input type="text" name="name" class="form-control" id="exampleInputName" placeholder="name"/>
-
-    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="password"/>
+                    <input type="text" name="name" className="form-control" id="exampleInputName" placeholder="name"/>
 
 
-  <button type="submit" class="btn btn-default">Sign in</button>
-  </div>
-</form>
+
+                    <input type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="password"/>
+                    </div>
+                  <button type="submit" className="btn btn-default">Sign in</button>
+
+                </form>
+
             </div>
         );
     }
 });
-
+var Footer = React.createClass({
+    render: function(){
+        let style={
+            width:'100%',
+            position: 'fixed',
+            height: '100px',
+            bottom:0,
+            float:'center',
+            backgroundColor:'#EEEEEE',
+            fontSize:'15px',
+            fontColor:'#424242',
+            padding:'10px',
+            fontFamily:'inherit'
+        }
+        return(
+            <div style={style}>
+                <div >
+                <p>LICENSE</p>
+                <p>OPEN SOURCE</p>
+                <p>GITHUB LINK</p>
+                </div>
+                </div>);
+    }
+});
         React.render(
         <Sign_in/>
         ,document.getElementById('sign_in'));
+             React.render(
+        <Body/>
+        ,document.getElementById('body'));
+          React.render(
+        <Footer/>
+        ,document.getElementById('footer'));
--- static/login.js
+++ static/login.js
@@ -25,27 +25,36 @@
 
 var Sign_up = React.createClass({
     render: function () {
+        let style={
+            float:'right',
+            marginRight:'100px',
+
+            className:'form-group',
+
+
+
+        }
 
         return (
-            <div>
+            <div style={style}>
                 <h1>Sign Up</h1>
                 <form action="/auth/signup" method="POST">
-                    <div class="form-group">
+                    <div className="form-group">
                         <label for="exampleInputName">Name</label>
-                        <input type="text" name="name" class="form-control" id="exampleInputName" placeholder="name"/>
+                        <input type="text" name="name" className="form-control" id="exampleInputName" placeholder="name"/>
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="password" name="password" class="form-control" id="exampleInputPassword1"
+                        <input type="password" name="password" className="form-control" id="exampleInputPassword1"
                                placeholder="password"/>
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label for="exampleInputPassword2">Password confirm</label>
-                        <input type="password" name="password_confirm" class="form-control" id="exampleInputPassword2"
+                        <input type="password" name="password_confirm" className="form-control" id="exampleInputPassword2"
                                placeholder="password confirm"/>
                     </div>
 
-                    <button type="submit" class="btn btn-default navbar-btn">Sign up</button>
+                    <button type="submit" className="btn btn-default navbar-btn">Sign up</button>
                 </form>
 
             </div>
