7f2517a62480e9160c2a3da34ee0c9fe8f805001
dms613412 <dms613412@naver.com>
2017-08-03 20:17:30 +0900

    except confirm password

--- static/login.js
+++ static/login.js
@@ -1,44 +1,47 @@
-
-
 var Sign_up = React.createClass({
     render: function () {
-        let style={
-            float:'right',
-            marginRight:'100px',
-            paddingTop:'10px',
-            className:'form-group',
-            
-
+        let style = {
+            float: 'right',
+            marginRight: '100px',
+            paddingTop: '10px'
         }
-
         return (
             <div style={style}>
                 <h1>Sign Up</h1>
-                <form action="/auth/signup" method="POST">
+                <form action="/auth/signup" method="POST" id="react_sign_up">
                     <div className="form-group">
-                        <label for="exampleInputName">Name</label>
-                        <input type="text" name="name" className="form-control" id="exampleInputName" placeholder="name"/>
+                        <label >Name</label>
+                        <input type="text" name="name" className="form-control" id="exampleInputName"
+                               placeholder="name"/>
                     </div>
                     <div className="form-group">
-                        <label for="exampleInputPassword1">Password</label>
+                        <label >Password</label>
                         <input type="password" name="password" className="form-control" id="exampleInputPassword1"
                                placeholder="password"/>
                     </div>
                     <div className="form-group">
-                        <label for="exampleInputPassword2">Password confirm</label>
-                        <input type="password" name="password_confirm" className="form-control" id="exampleInputPassword2"
-                               placeholder="password confirm"/>
+                        <label >Password confirm</label>
+                        <input type="password" name="password_confirm" className="form-control"
+                               id="exampleInputPassword2"
+                               placeholder="password confirm" title="Please enter the same Password as above"/>
                     </div>
-
                     <button type="submit" className="btn btn-default navbar-btn">Sign up</button>
+
+                    {console.log(document.getElementById('exampleInputPassword1'))}
                 </form>
 
+
+                { ( (document.getElementById('exampleInputPassword1').valueOf()!==null) ?
+                    (( document.getElementById('exampleInputPassword1').valueOf() !== document.getElementById('exampleInputPassword2').valueOf()) ?
+                        alert("Passwords Don't Match") : alert('success')) : '')}
+                {console.log(document.getElementById('exampleInputPassword1'))}
+
+                {console.log(document.getElementById('exampleInputPassword1')).valueOf()}
             </div>
         );
     }
 });
 
-
-        React.render(
-        <Sign_up/>
-        ,document.getElementById('sign_up'));
+React.render(
+    <Sign_up/>
+    , document.getElementById('sign_up'));
