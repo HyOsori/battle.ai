58a29faacd175f56c443ceb0b574e12b3e889747
dms613412 <dms613412@naver.com>
2017-08-17 20:42:19 +0900

    login_confirm_alert

--- static/login.js
+++ static/login.js
@@ -1,4 +1,24 @@
 var Sign_up = React.createClass({
+    getInitialState: function () {
+        return {
+            name: '',
+            password: '',
+            password_confirm: ''
+        };
+    },
+    componentDidMount: function() {
+        function validateSignup() {
+            alert("A");
+            return this.state.password === this.state.password_confirm;
+        }
+    },
+    handleChange: function (e) {
+        let nextState = {};
+        if (typeof e !== 'object') return ;
+        nextState[e.target.name] = e.target.value;
+        this.setState(nextState);
+
+    },
     render: function () {
         let style = {
             float: 'right',
@@ -9,33 +29,27 @@ var Sign_up = React.createClass({
         return (
             <div style={style}>
                 <h1>Sign Up</h1>
-                <form action="/auth/signup" method="POST" id="react_sign_up">
+
+                <form action="/auth/signup" method="POST" id="react_sign_up" onsubmit="return validateSignup()">
                     <div className="form-group">
                         <label >Name</label>
-                        <input type="text" name="name" className="form-control" id="exampleInputName"
-                               placeholder="name"/>
+                        <input type="text" name="name" className="form-control" id="exampleInputName" value={this.state.name} placeholder="name" onChange={this.handleChange}/>
                     </div>
                     <div className="form-group">
                         <label >Password</label>
-                        <input type="password" name="password" className="form-control" id="exampleInputPassword1"
-                               placeholder="password"/>
+                        <input type="password" name="password" className="form-control" id="exampleInputPassword1" value={this.state.password}
+                               placeholder="password" onChange={this.handleChange}/>
                     </div>
                     <div className="form-group">
                         <label >Password confirm</label>
-                        <input type="password" name="password_confirm" className="form-control"
+                        <input type="password" name="password_confirm" className="form-control" value={this.state.password_confirm}
                                id="exampleInputPassword2"
-                               placeholder="password confirm" title="Please enter the same Password as above"/>
+                               placeholder="password confirm" title="Please enter the same Password as above" onChange={this.handleChange}/>
                     </div>
                     <button type="submit" className="btn btn-default navbar-btn">Sign up</button>
 
-                    {console.log(document.getElementById('exampleInputPassword1'))}
                 </form>
-
-                    {(document.getElementById('exampleInputPassword1')!== null) ?
-                        ( document.getElementById('exampleInputPassword1').value !== document.getElementById('exampleInputPassword2').value) ?
-                            alert("Passwords Don't Match") : alert("success") : ''}
-                {console.log(document.getElementById('exampleInputPassword1'))}
-
+                {(this.state.password === this.state.password_confirm)? alert("A"): alert("B")}
 
             </div>
         );
